421618c7ef5806d1a52b64577315616f58629e80
Johan Esteves <jsesteves91@gmail.com>
2017-10-04 02:10:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart.size == 0
-        put "Your cart is empty"
+        puts "Your cart is empty"
       else
         @@cart.each do |item|
           resp.write "#{item}\n"
