48709fa81b4c1640a2c5c6fd109a521cf449f08f
Johan Esteves <jsesteves91@gmail.com>
2017-10-04 02:10:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,8 @@ class Application
         "Your cart is empty"
       else
         @@cart.each do |item|
-        resp.write "#{item}\n"
+          resp.write "#{item}\n"
+        end
       end
     elsif req.path.match(/add/)
       add_item = req.parms["q"]
